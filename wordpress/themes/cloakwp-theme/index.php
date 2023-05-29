f0f4aad99dea650cfe7cbc28139c250f9dad8d24
Kaelan Smith <kaelancsmith@gmail.com>
2023-05-28 21:58:26 -0700

    comment out redirect to REST API until we figure out best choice

--- wordpress/themes/cloakwp-theme/index.php
+++ wordpress/themes/cloakwp-theme/index.php
@@ -2,18 +2,17 @@
 /**
  * Redirect frontend requests to REST API.
  *
- * @package  Postlight_Headless_WP
  */
 
 // Redirect individual posts to the REST API endpoint.
-if ( is_singular() ) {
-    header(
-        sprintf(
-            'Location: /wp-json/wp/v2/%s/%s',
-            get_post_type_object( get_post_type() )->rest_base,
-            get_post()->ID
-        )
-    );
-} else {
-    header( 'Location: /wp-json/' );
-}
+// if ( is_singular() ) {
+//     header(
+//         sprintf(
+//             'Location: /wp-json/wp/v2/%s/%s',
+//             get_post_type_object( get_post_type() )->rest_base,
+//             get_post()->ID
+//         )
+//     );
+// } else {
+//     header( 'Location: /wp-json/' );
+// }
