55b2441485e8e2ee97c6fb191c5011b89523cfbd
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-05 10:59:11 -0700

    add fetchOptions argument to getPost, and set default val for includeJwt to false

--- packages/cloakwp/lib/getters/getPost.ts
+++ packages/cloakwp/lib/getters/getPost.ts
@@ -1,14 +1,14 @@
 import { getQueryParamsString } from "../utils/getQueryParamsString";
 import { useFetchRestAPI } from "../hooks/useFetchRestAPI";
 
-export async function getPost({ postType = 'posts', slug, id, queryParams = '' }) {
+export async function getPost({ postType = 'posts', slug, id, queryParams = '', fetchOptions = {} }) {
   let endpoint
   if (id) endpoint = `/${postType}/${id}`
   else endpoint = (slug && slug != '/') ? `/${postType}?slug=${slug}` : `/frontpage`
   
   let queryParamsString = getQueryParamsString(queryParams, endpoint)
 
-  let data = await useFetchRestAPI(`${endpoint}${queryParamsString}`);
+  let data = await useFetchRestAPI(`${endpoint}${queryParamsString}`, { includeJwt: false, ...fetchOptions });
   if (Array.isArray(data)) data = data[0]
   return { data }
 }
