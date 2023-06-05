77cf7987d1b2b9b6d6d9e83bc0aebef61a2c063a
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-05 10:56:25 -0700

    delete old cloakwp api routes

--- examples/basic-starter/src/pages/api/exit-preview.js
+++
@@ -1,9 +0,0 @@
-export default async function exit(req, res) {
-    // Exit the current user from "Preview Mode". This function accepts no args.
-    const { parentSlug } = req.query;
-    res.clearPreviewData()
-  
-    // Redirect the user back to the index page.
-    res.writeHead(307, { Location: `/${parentSlug}` })
-    res.end()
-}  
--- examples/basic-starter/src/pages/api/revalidate/[slug].js
+++
@@ -1,4 +0,0 @@
-import { regenerateStaticPage } from 'cloakwp'
-
-const handler = async (req, res) => regenerateStaticPage(req, res)
-export default handler
