4c56963a7fd00234de44918d9b9e10de4e83c537
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-08 11:17:11 -0700

    add requireGlob function and use it to include all blocks + cpts, also add BrowserSync script to admin pages

--- wordpress/themes/cloakwp-child-theme/functions.php
+++ wordpress/themes/cloakwp-child-theme/functions.php
@@ -1,24 +1,63 @@
 <?php
 
-require_once get_stylesheet_directory() . '/models/testimonials.php';
+
+
+function requireGlob($glob)
+{
+  $folder_path = get_stylesheet_directory() . $glob;
+  $files = glob($folder_path);
+
+  foreach ($files as $file) {
+    require_once $file;
+  }
+}
+
+requireGlob('/models/*.php');
+requireGlob('/blocks/*/*.php');
+
+// require_once get_stylesheet_directory() . '/blocks/testimonials.php';
+
+add_action('admin_head', 'add_custom_script_to_admin');
+function add_custom_script_to_admin()
+{
+  echo '<script id="__bs_script__">//<![CDATA[
+    (function() {
+      try {
+        console.log("adding BrowserSync script");
+        var script = document.createElement("script");
+        if ("async") {
+          script.async = true;
+        }
+        script.src = "http://localhost:3000/browser-sync/browser-sync-client.js?v=2.29.3";
+        if (document.body) {
+          document.body.appendChild(script);
+        } else if (document.head) {
+          document.head.appendChild(script);
+        }
+      } catch (e) {
+        console.error("Browsersync: could not append script tag", e);
+      }
+    })()
+  //]]></script>';
+}
 
 add_action('enqueue_block_editor_assets', 'gutenberg_editor_assets');
 function gutenberg_editor_assets()
 {
   // Load our custom styles for the Gutenberg editor:
   wp_enqueue_style('my-gutenberg-editor-styles', get_theme_file_uri('/blocks/styles.css'), array(), filemtime('/blocks/styles.css'), 'all');
-  
+
   // Load our custom JS scripts that modify/strip the Gutenberg editor in opinionated ways:
   wp_enqueue_script('gutenberg-editor-js', get_theme_file_uri('/blocks/scripts.js'), array(), filemtime('/blocks/scripts.js'), true);
+
 }
 
 add_theme_support('post-thumbnails'); // enable featured images
 add_post_type_support('page', 'excerpt'); // enable page excerpts
 
 /*
-  Register whatever menus your project needs here. This is required in 
-  order for WP Admin > Appearance > Menus page to be visible for new 
-  Block themes such as this one. 
+  This is required in order for WP Admin > Appearance > Menus page to 
+  be visible for new Block themes such as this one. 
 */
 add_action('init', 'register_menus');
 function register_menus()
@@ -39,7 +78,7 @@ function register_blocks()
   register_block_type(get_template_directory() . '/blocks/postList/block.json');
   register_block_type(get_template_directory() . '/blocks/pageHeader/block.json');
   register_block_type(get_template_directory() . '/blocks/testimonial/block.json');
-  register_block_type(get_template_directory() . '/blocks/cardFeature/block.json');
+  // register_block_type(get_template_directory() . '/blocks/cardFeature/block.json');
   register_block_type(get_template_directory() . '/blocks/faq/block.json');
   register_block_type(get_template_directory() . '/blocks/cta/block.json');
 }
@@ -74,7 +113,8 @@ function wpd_change_post_type_args($args, $post_type)
 }
 
 // Give editors access to the Menu tab
-function allow_menu_editing_for_editors() {
+function allow_menu_editing_for_editors()
+{
   $role = get_role('editor');
   $role->add_cap('edit_theme_options');
 }
@@ -85,10 +125,11 @@ add_action('admin_init', 'allow_menu_editing_for_editors');
   Remove "Comments" from wp-admin sidebar for all roles.
   Remove "Tools", "Dashboard", and "Yoast SEO" for non-admins
 */
-function remove_admin_pages(){
+function remove_admin_pages()
+{
   remove_menu_page('edit-comments.php');
 
-  if (!current_user_can( 'administrator' )) { // remove certain pages for non-administrators
+  if (!current_user_can('administrator')) { // remove certain pages for non-administrators
     remove_menu_page('tools.php'); // remove "Tools"
     remove_menu_page('index.php'); // remove "Dashboard"
 
@@ -97,15 +138,16 @@ function remove_admin_pages(){
     remove_menu_page('wpseo_workouts');
   }
 }
-add_action( 'admin_menu', 'remove_admin_pages' );
+add_action('admin_menu', 'remove_admin_pages');
 
 /*
   Function to remove various options in wp-admin top toolbar (not sidebar)
   Currently used to remove the "Comments" and "View Posts" menu items
 */
-function remove_admin_toolbar_options() {
+function remove_admin_toolbar_options()
+{
   global $wp_admin_bar;
   $wp_admin_bar->remove_menu('comments');
   $wp_admin_bar->remove_menu('archive');
 }
-add_action( 'wp_before_admin_bar_render', 'remove_admin_toolbar_options' );
+add_action('wp_before_admin_bar_render', 'remove_admin_toolbar_options');
