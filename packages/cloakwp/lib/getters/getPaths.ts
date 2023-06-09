678b92955852e99a7afd25e1e6a2ba85a4270e80
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-09 16:55:30 -0700

    Greatly simplify getPaths thanks to new "pathname" prop in REST API

--- packages/cloakwp/lib/getters/getPaths.ts
+++ packages/cloakwp/lib/getters/getPaths.ts
@@ -1,19 +1,9 @@
-import { useFetchRestAPI } from "../hooks/useFetchRestAPI";
 import { getPosts } from "./getPosts";
 
-export async function getPaths(postType = 'pages', prependSlug = false) {
-    // const { postType = 'pages', prependSlug = false } = props || {}
-    let home
-    if(postType == 'pages') home = await useFetchRestAPI('/frontpage')
-
-    const posts = await getPosts(postType);
-
-    const prepend = prependSlug ? (
-        typeof prependSlug == "boolean" ? `/${postType}` : `/${prependSlug}` // you can either set prependSlug to true/false or a custom string -- if true, we'll use the postType string as the prepend string
-    ) : ''
-
-    return posts?.map((post) => {
-        const slug = (postType == 'pages' && post.slug == home?.slug) ? '' : post.slug
-        return `${prepend}/${slug}`
-    }) || [];
+export async function getPaths(postType = 'pages') {
+  const posts = await getPosts(postType, {
+    queryParams: { '_fields': 'pathname' }
+  });
+  
+  return posts?.map(post => post.pathname) || [];
 }
