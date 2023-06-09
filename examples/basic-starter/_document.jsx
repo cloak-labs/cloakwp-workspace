5b0e7bf1403500886e443240a04e9977f46e9bf1
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-09 16:53:08 -0700

    change lang to en-US

--- examples/basic-starter/_document.jsx
+++ examples/basic-starter/_document.jsx
@@ -3,9 +3,9 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en-CA">
+      <Html lang="en-US">
         <Head>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
         </Head>
         <body>
           <Main />
