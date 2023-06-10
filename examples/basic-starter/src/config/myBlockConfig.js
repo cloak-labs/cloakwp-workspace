ff6f3843da7502e4b797256ff548d0cb4899ca51
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-09 20:07:31 -0700

    Add acf/hero block to config

--- examples/basic-starter/src/config/myBlockConfig.js
+++ examples/basic-starter/src/config/myBlockConfig.js
@@ -4,10 +4,12 @@ import {
   FaqsBlock,
   ButtonBlock,
   H4Block,
+  HeroBlock
 } from '@/components/Blocks'
 
 const myBlockConfig = {
-  'core/heading': { // note: we can override/extend core blocks like so.. here we aren't overriding the heading block's Component or anything else, we're only extending it by adding custom props -- behind the scenes, it does a deep object merge rather than a full object override 
+  // We can override/extend how core blocks render like so:
+  'core/heading': { // here we aren't overriding the heading block's Component or anything else, we're only extending it by adding custom props -- behind the scenes, it does a deep object merge rather than a full object override 
     props: { // you can provide "props" where the children properties will get applied as component props to the associated Block component  
       tags: {
         4: H4Block // the core/heading block has a built-in unique prop where we can override how each heading level gets rendered (here we're providing a custom Block Wrapper component specifically for H4 headings)
@@ -19,7 +21,12 @@ const myBlockConfig = {
     component: ButtonBlock // here's an example of totally overriding a core block's component 
   },
   // below we map our custom ACF Blocks to our custom components
-  'acf/cardsgrid': {
+  'acf/hero': {
+    component: HeroBlock,
+    container: false,
+    // containerClasses: 'py-12',
+  },
+  'acf/cards': {
     component: CardsGridBlock,
     container: true,
     containerClasses: 'py-12',
