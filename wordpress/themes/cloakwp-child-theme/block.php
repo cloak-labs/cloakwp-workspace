70d4e45eedc6edf8b6d3b5a795adeecbd1955ed6
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-10 17:57:58 -0700

    delete old block.php

--- wordpress/themes/cloakwp-child-theme/block.php
+++
@@ -1,4 +0,0 @@
-<div style="border: 1px solid gray; border-radius: 5px; background-color: #efefef; padding: 20px; color: red;">
-  Previews for custom blocks are not supported; please switch back to edit mode.
-</div>
-<?php
