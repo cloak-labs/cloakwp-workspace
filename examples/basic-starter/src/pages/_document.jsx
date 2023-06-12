277ac586b27f890bf9c5283ee4966824c971a1a3
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-12 11:02:38 -0700

    adjust document styles

--- examples/basic-starter/src/pages/_document.jsx
+++ examples/basic-starter/src/pages/_document.jsx
@@ -2,9 +2,9 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className="h-full antialiased" lang="en">
+    <Html lang="en">
       <Head />
-      <body className="flex h-full flex-col">
+      <body className="antialiased h-full">
         <Main />
         <NextScript />
       </body>
