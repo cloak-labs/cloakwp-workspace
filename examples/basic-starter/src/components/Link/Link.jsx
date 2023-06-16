a66e004543cfaee6c28106c9f00040c560db9e7d
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-15 17:24:58 -0700

    slightly adjust CustomLink

--- examples/basic-starter/src/components/Link/Link.jsx
+++ examples/basic-starter/src/components/Link/Link.jsx
@@ -6,11 +6,12 @@ const CustomLink = ({
   href,
   ref,
   children,
+  openInNewTab = true,
   ...rest
 }) => {
   if(!href) return <span ref={ref} {...rest}>{children}</span>
 
-  const isInternalLink = href && href.startsWith('/')
+  const isInternalLink = href && href.startsWith('/') && !href.startsWith('/api/')
   const isAnchorLink = href && href.startsWith('#')
 
   if (isInternalLink) {
@@ -27,7 +28,7 @@ const CustomLink = ({
 
   if(!href.startsWith('/') && !href.startsWith('http') && !href.startsWith('mailto:') && !href.startsWith('tel:')) href = `https://${href}`
 
-  return <a target="_blank" rel="noopener noreferrer" ref={ref} href={href} {...rest}>{children}</a>
+  return <a target={openInNewTab ? "_blank" : ""} rel="noopener noreferrer" ref={ref} href={href} {...rest}>{children}</a>
 }
 
 export default CustomLink
