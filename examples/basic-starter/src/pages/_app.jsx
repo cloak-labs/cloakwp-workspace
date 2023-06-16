e23b70a194b409746a30e12510d587b4f6c288a3
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-15 17:25:18 -0700

    add yoast_head to all pages

--- examples/basic-starter/src/pages/_app.jsx
+++ examples/basic-starter/src/pages/_app.jsx
@@ -3,12 +3,20 @@ import 'focus-visible'
 import { Layout } from '@/components/Layout'
 import { BlockConfigProvider } from 'cloakwp'
 import myBlockConfig from '@/config/myBlockConfig'
+import Head from 'next/head'
+import parse from 'html-react-parser';
 
 export default function App({ Component, pageProps }) {
-  const { enableLayout = true } = pageProps
+  const { enableLayout = true, pageData } = pageProps
+  const { yoast_head } = pageData || {}
 
   return (
     <BlockConfigProvider blocks={myBlockConfig}>
+      {yoast_head &&
+        <Head>
+          {parse(yoast_head)}
+        </Head>
+      }
       {enableLayout ? (
         <Layout {...pageProps}>
           <Component {...pageProps} />
