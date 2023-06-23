8d5243862928632a340d7b7d3893e0e85e866d0a
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-22 18:09:38 -0700

    add more file types to browserSync watch list

--- bs-config.js
+++ bs-config.js
@@ -2,7 +2,11 @@ module.exports = {
   files: [
     "./wordpress/themes/**/*.php",
     "./wordpress/themes/**/*.json",
+    "./wordpress/themes/**/*.css",
+    "./wordpress/themes/**/*.js",
     "./wordpress/plugins/**/*.php",
+    "./wordpress/plugins/**/*.css",
+    "./wordpress/plugins/**/*.js",
     // Add more directories as needed
   ],
 };
