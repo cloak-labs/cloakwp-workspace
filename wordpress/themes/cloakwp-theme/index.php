8694324d9e2b3dbd83ed1f03d4b6625e17171a92
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-27 09:13:41 -0700

    update index.php to redirect front-end routes to wp-admin

--- wordpress/themes/cloakwp-theme/index.php
+++ wordpress/themes/cloakwp-theme/index.php
@@ -1,18 +1,7 @@
 <?php
 /**
- * Redirect frontend requests to REST API.
- *
+ * Redirect WordPress frontend requests to wp-admin.
  */
 
-// Redirect individual posts to the REST API endpoint.
-// if ( is_singular() ) {
-//     header(
-//         sprintf(
-//             'Location: /wp-json/wp/v2/%s/%s',
-//             get_post_type_object( get_post_type() )->rest_base,
-//             get_post()->ID
-//         )
-//     );
-// } else {
-//     header( 'Location: /wp-json/' );
-// }
+wp_redirect(site_url('/wp-admin'), 301);
+exit;
