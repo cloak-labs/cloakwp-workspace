7c985b22e49c0d2e4ce36cb0482c813c55748332
Kaelan Smith <kaelancsmith@gmail.com>
2023-06-27 09:13:21 -0700

    move most theme functions to plugin

--- wordpress/themes/cloakwp-theme/functions.php
+++ wordpress/themes/cloakwp-theme/functions.php
@@ -1,56 +1,16 @@
 <?php
 
-add_action('enqueue_block_editor_assets', 'cloakwp_gutenberg_editor_assets');
-function cloakwp_gutenberg_editor_assets()
-{
-  // Load our custom CSS stylesheets for the Gutenberg editor:
-  wp_enqueue_style('cloakwp-gutenberg-editor-styles', get_theme_file_uri('/css/gutenberg-styles.css'), array(), filemtime('/css/gutenberg-styles.css'), 'all');
+use CloakWP\Utils;
 
-  // Load our custom JS scripts for the Gutenberg editor:
-  wp_enqueue_script('acf-block-iframe-preview-js', get_theme_file_uri('/js/acf-block-iframe-preview.js'), array(), filemtime('/js/acf-block-iframe-preview.js'), true);
-}
+/* 
+  This function is used as the renderCallback (defined in block.json) for ACF Blocks, 
+  enabling iframe previews of your decoupled front-end's associated React components.
 
-remove_theme_support( 'core-block-patterns' );
-
-/*
-  Expand ACF field data returned in REST API; eg. image fields return full image data rather than just an ID. More info: https://www.advancedcustomfields.com/resources/wp-rest-api-integration/
+  It requires that you also install the CloakWP Plugin, and use the CloakWP NPM Package
+  on your decoupled front-end.
 */
-add_filter('acf/settings/rest_api_format', function () {
-  return 'standard';
-});
-
-/*
-  Change the JWT token issuer:
-*/
-add_filter('jwt_auth_iss', function () {
-  // Default value is get_bloginfo( 'url' );
-  return site_url();
-});
-
-/*
-  Add ability for "editor" user role to edit WP Menus, but hide all other submenus under Appearance (for editors only) -- eg. we don't want clients to be able to switch/deactivate theme and break site 
-*/
-function restrict_appearance_menu_for_editors() {
-  $role_object = get_role( 'editor' );
-  if(!$role_object->has_cap('edit_theme_options')){
-    $role_object->add_cap( 'edit_theme_options' );
-  }
-
-  if (current_user_can('editor')) { // remove certain Appearance > Sub-pages
-      remove_submenu_page( 'themes.php', 'themes.php' ); // hide the theme selection submenu
-      remove_submenu_page( 'themes.php', 'widgets.php' ); // hide the widgets submenu
-
-      // special handling for removing "Customize" submenu (above method doesn't work due to its URL structure) --> snippet taken from https://stackoverflow.com/a/50912719/8297151
-      global $submenu;
-      if ( isset( $submenu[ 'themes.php' ] ) ) {
-          foreach ( $submenu[ 'themes.php' ] as $index => $menu_item ) {
-              foreach ($menu_item as $value) {
-                  if (strpos($value,'customize') !== false) {
-                      unset( $submenu[ 'themes.php' ][ $index ] );
-                  }
-              }
-          }
-      }
+function render_acf_block_iframe_preview ($block, $content, $is_preview, $post_id, $wp_block, $context) {
+  if (class_exists('CloakWP\Utils')) {
+    include(Utils::cloakwp_plugin_path() . '/block-preview.php');
   }
 }
-add_action('admin_head', 'restrict_appearance_menu_for_editors');
