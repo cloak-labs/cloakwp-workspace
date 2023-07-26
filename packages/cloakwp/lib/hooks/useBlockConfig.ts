c4367512ea8ac301babd91600293a034ba60369c
Kaelan Smith <kaelancsmith@gmail.com>
2023-07-26 13:15:59 -0700

    delete cloakwp package before bringing it as via meta

--- packages/cloakwp/lib/hooks/useBlockConfig.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { BlockConfigContext } from "../context/blockConfig";
-
-export function useBlockConfig() {
-  let config = useContext(BlockConfigContext)
-	return config
-}
